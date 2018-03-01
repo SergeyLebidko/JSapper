bfb845a3384d08acbff0dfd6c468c374c12e78eb
SergeyLebidko <it.sergeyler@mail.ru>
2018-03-01 21:41:06 +0300

    Простые эксперименты с созданием игрового поля

--- src/jsapper/Cell.java
+++ src/jsapper/Cell.java
@@ -2,21 +2,25 @@ package jsapper;
 
 import javax.swing.*;
 import java.awt.*;
-import java.beans.Transient;
 
 public class Cell extends JPanel{
+
+    private Color darkColor=new Color(120,120,120);
+    private Color lightColor=new Color(250,250,250);
+
+
 
     @Override
     protected void paintComponent(Graphics g) {
         Graphics2D g2;
         g2=(Graphics2D)g;
 
-        g2.setColor(new Color(150, 150, 150));
+        int r=(int)(Math.random()*100);
+        if(r<50)g2.setColor(darkColor); else g2.setColor(lightColor);
 
         int w=this.getSize().width;
         int h=this.getSize().height;
-
         g2.fillRect(0,0, w, h);
-
     }
+
 }
--- src/jsapper/MainClass.java
+++ src/jsapper/MainClass.java
@@ -1,21 +1,33 @@
 package jsapper;
 
 import javax.swing.*;
-import javax.swing.border.EtchedBorder;
 import java.awt.*;
 
 public class MainClass {
 
-    private JFrame frm;
-    private int W=800;
-    private int H=600;
-    private Cell c;
+    private JFrame frm;                                       //Окно
+    private int W;                                            //Щирина окна (в пикселях)
+    private int H;                                            //Высота окна (в пикселях)
+    private int wCellCount=20;                                //Ширина окна (в клетках)
+    private int hCellCount=15;                                //Высота окна (в клетках)
+    private int borderW=2;                                    //Горизонтальный отступ (в пикселях)
+    private int borderH=2;                                    //Вертикальный отступ (в пикселях)
+    private int cellW=48;                                     //Ширина клетки (в пикселях)
+    private int cellH=48;                                     //Высота клетки (в пикселях)
+    private int correctionW=6;                                //Горизонтальная поправка высоты окна
+    private int correctionH=29;                               //Вертикальная поправка высоты окна
+    private Cell[][] cells;                                   //Массив Клеток
+    private Color backWindowColor =Color.WHITE;               //Цвет фона окна
+    private Color borderCellColor=new Color(50,50,50); //Цвет границы ячеек
 
     public MainClass() {
+        //Формируем окно и выставляем его по центру экрана
         frm = new JFrame("JSapper");
         frm.setLayout(null);
         frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frm.getContentPane().setBackground(new Color(10,100,200));
+        frm.getContentPane().setBackground(backWindowColor);
+        W=wCellCount*(borderW+cellW+borderW)+correctionW;
+        H=hCellCount*(borderH+cellH+borderH)+correctionH;
         frm.setSize(W, H);
         int xPos= (int)(Toolkit.getDefaultToolkit().getScreenSize().getWidth()/2-W/2);
         int yPos= (int)(Toolkit.getDefaultToolkit().getScreenSize().getHeight()/2-H/2);
@@ -23,10 +35,15 @@ public class MainClass {
         frm.setResizable(false);
         frm.setVisible(true);
 
-        c=new Cell();
-        c.setBounds(100,100,32,32);
-        c.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
-        frm.add(c);
+        //Формируем массив клеток и добавляем их на форму
+        cells=new Cell[hCellCount][wCellCount];
+        for(int i=0;i<hCellCount;i++)
+            for(int j=0;j<wCellCount;j++){
+                cells[i][j]=new Cell();
+                cells[i][j].setBounds(j*(borderW+cellW+borderW)+borderW,i*(borderH+cellH+borderH)+borderH,cellW,cellH);
+                cells[i][j].setBorder(BorderFactory.createLineBorder(borderCellColor));
+                frm.add(cells[i][j]);
+            }
     }
 
     public static void main(String[] args) {
